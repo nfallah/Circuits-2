8a8e9679744d89c415461b2cbefd63608c03b86e
nfallah <nfallah399@gmail.com>
2023-01-27 00:58:45 -0500

    Project import + gate script creation/basics

---
+++ Assets/Script/Gate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate
+{
+    // Include buffer?
+    public enum GateType { AND, CLOCK, DISPLAY, OR, LEVER, NOT, SPLIT, XOR }
+
+    public GateType type;
+
+    public bool[] input, output;
+
+    private bool activationCondition;
+
+    public Gate(GateType type)
+    {
+        this.type = type;
+
+        switch(type)
+        {
+            case GateType.AND:
+
+            case GateType.DISPLAY:
+
+            case GateType.OR:
+        }
+    }
+}
